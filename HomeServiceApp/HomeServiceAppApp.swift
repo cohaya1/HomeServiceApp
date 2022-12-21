5400f209a0908743e6bba9a4a5d8a1fecdfc1b5d
Chika Ohaya <chika.ohaya@gmail.com>
2022-12-21 16:16:45 -0500

    Initial Commit

---
+++ HomeServiceApp/HomeServiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeServiceAppApp.swift
+//  HomeServiceApp
+//
+//  Created by Chika Ohaya on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct HomeServiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
